886ecee6ce14b38f0695e59653be1087ace84f89
gabbs <gortman@rednovalabs.com>
2021-01-29 13:21:21 -0600

    add react app and webpack template

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const { NODE_ENV } = process.env;
+
+module.exports = {
+  entry: {
+    main: './website/index.js',
+  },
+  mode: NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // https://webpack.js.org/loaders/sass-loader/
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // https://webpack.js.org/loaders/sass-loader/#additionaldata
+              additionalData: '@import "default";',
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, 'website/theme/')],
+              },
+            },
+          },
+        ],
+      },
+      {
+        // https://webpack.js.org/loaders/file-loader/
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/',
+    hotOnly: true,
+  },
+  // clean /dist with clean-webpack-plugin
+  plugins: [
+    NODE_ENV === 'development' && new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+      // favicon: path.join(__dirname, 'public/favicon.ico'),
+    }),
+  ].filter(Boolean),
+};
---
+++ website/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<span>new app</span>, document.getElementById('root'));
